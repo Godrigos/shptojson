08c23c0e93afa58f1b3956fc867c7586d9aa821e
Rodrigo Aluizio <r.aluizio@gmail.com>
2022-01-04 12:11:21 -0300

    Removed windows target. For Now.

--- build.sh
+++ build.sh
@@ -3,35 +3,12 @@
 # Attention to conan profiles when compiling or cross compiling
 
 echo "Linux compilation." 
-cd Linux
-
-if [ -d "build" ]; then
-  echo "build directory already exists."
-else
-  mkdir build
-fi
+cd build
 
 cd build
 conan install .. --build=missing --profile ../linux
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --clean-first
 
+cd ..
 echo "Linux compilation ended."
-echo
-
-echo "Windows cross compilation."
-cd ../../Windows
-
-if [ -d "build" ]; then
-  echo "build directory already exists."
-else
-  mkdir build
-fi
-
-conan install .. --build=missing --profile -pr:h ../windows -pr:b ../../Linux/linux \
-  -o libcurl:with_unix_sockets=False
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --clean-first
-cd ../..
-
-echo "Windows compilation ended."
