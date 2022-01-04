47c8e6720233b5ad513fdb7b8a523b815729b01d
Rodrigo Aluizio <r.aluizio@gmail.com>
2022-01-04 11:51:57 -0300

    separated conan builds

--- build.sh
+++ build.sh
@@ -3,20 +3,35 @@
 # Attention to conan profiles when compiling or cross compiling
 
 echo "Linux compilation." 
+cd Linux
+
+if [ -d "build" ]; then
+  echo "build directory already exists."
+else
+  mkdir build
+fi
 
 cd build
-conan install .. --build=missing --profile ../Linux/linux
+conan install .. --build=missing --profile ../linux
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --clean-first
 
 echo "Linux compilation ended."
 echo
 
-echo "Windows cross compilation." 
+echo "Windows cross compilation."
+cd ../../Windows
 
-conan install .. --build=missing --profile ../Windows/windows
+if [ -d "build" ]; then
+  echo "build directory already exists."
+else
+  mkdir build
+fi
+
+conan install .. --build=missing --profile -pr:h ../windows -pr:b ../../Linux/linux \
+  -o libcurl:with_unix_sockets=False
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --clean-first
-cd ..
+cd ../..
 
 echo "Windows compilation ended."
